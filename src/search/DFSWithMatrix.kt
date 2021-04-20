c63a26447ba93b138e83dfd6b5e9503d8c85d627
june <brainyjun@gmail.com>
2021-04-20 22:13:31 +0900

    17 DFS(Depth First Search) 깊이 우선 탐색 - 인접 행렬과 재귀함수를 이용하여

---
+++ src/search/DFSWithMatrix.kt
@@ -0,0 +1,53 @@
+package search
+
+// Depth First Search - 깊이 우선 탐색
+// 인접 행렬과 재귀함수를 이용하여
+
+private val vertexCount = 7
+private var visited : BooleanArray = booleanArrayOf()
+private var graph: Array<IntArray> = arrayOf()
+
+fun main() {
+
+    graph = Array(vertexCount + 1) { IntArray(vertexCount + 1) { 0 } }
+    visited = BooleanArray(graph.size) { false }
+
+    addGraph(1, 2)
+    addGraph(1, 3)
+    addGraph(2, 3)
+    addGraph(2, 4)
+    addGraph(2, 5)
+    addGraph(3, 6)
+    addGraph(3, 7)
+    addGraph(4, 5)
+    addGraph(6, 7)
+
+    dfs(1)
+}
+
+private fun addGraph(a: Int, b:Int) {
+    graph[a][b] = 1
+    graph[b][a] = 1
+}
+
+private fun dfs(index: Int) {
+    visited[index] = true
+    print("$index ")
+
+    val root = graph[index]
+
+    root.forEachIndexed { idx, isAdj ->
+        if (isAdj != 0 && visited[idx] == false) {
+            dfs(idx)
+        }
+    }
+
+//    for (x in root.indices) {
+//        val y = graph[index][x]
+//
+//        if (y != 0 && visited[x] == false) {
+//            dfs(x)
+//        }
+//
+//    }
+}
